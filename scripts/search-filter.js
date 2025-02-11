3a4ff97a104f1ffef4f92a1f4207fd86b5ebf465
Bruno Coitinho <bruno.pereira1988@gmail.com>
2025-02-11 11:11:17 -0300

    feat: navegação pelo nav-bottom e carregamento da página clicada finalizados

---
+++ scripts/search-filter.js
@@ -0,0 +1,157 @@
+const navList = document.querySelector('.bottom-nav__list')
+
+
+document.addEventListener('DOMContentLoaded', (e) =>{
+
+    
+    
+    
+    let paginaAtual = 1;
+    let paginaAntesDoClick = paginaAtual
+
+    
+    
+    
+    carregarDados().then((lobos) => {
+        construirPagina(paginaAtual, lobos)
+
+    });
+
+})
+
+async function carregarDados() {
+    try {
+        const response = await fetch("../lobinhos.json");
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Erro ao carregar JSON:", error);
+    }
+}
+
+function construirPagina(idPagina, dados){
+    const containerLobos = document.querySelector("#lobos-container");
+    containerLobos.innerHTML = ''
+    for (let i = idPagina * 4 - 4; i < 4 * idPagina; i++) {
+
+        if ((i+1) % 2 == 0){
+            reverse = 'reverse'
+        }else{
+            reverse = ''
+        }
+
+        containerLobos.innerHTML += `
+        <div class="card-example ${reverse}" id='${dados[i].id}'>
+                    <div class="image-container">
+                        <img src="${dados[i].imagem}">
+                    </div>
+                    <div class="content-container">
+                        <div class="title">
+                            <div>
+                                <h3>
+                                    ${dados[i].nome}
+                                </h3>
+                                <span>
+                                    ${dados[i].idade}
+                                </span>
+                            </div>
+                            <a href="adotar-lobinho.html" class="button" id='btnLobo${dados[i].id}'>
+                                Adotar
+                            </a>
+                        </div>
+
+                        <p class="description">
+                            ${dados[i].descricao}
+                        </p>
+                    </div>
+                </div>`
+    }
+
+    construirNavButtons(idPagina, dados.length)
+}
+
+function construirNavButtons(idPagina, numTotal){
+    paginaAntesDoClick = idPagina
+    navList.innerHTML = ''
+    let rangeMin = 1
+
+    if (idPagina != 1){
+        navList.innerHTML += `<li class="bottom-nav__item"><a class="bottom-nav__link" href="#" id='i'>&lt;&lt;</a></li>`
+    }
+
+
+    if (idPagina >3){
+        rangeMin = idPagina - 2
+        navList.innerHTML += `<li class="bottom-nav__item"><a class="bottom-nav__link" href="#" id='-'>...</a></li>`
+    }
+
+    let rangeMax = Math.min(rangeMin + 4,Math.ceil(numTotal/4))
+
+
+    
+    for (let i = rangeMin; i <= rangeMax; i++) {
+        navList.innerHTML += `<li class="bottom-nav__item"><a class="bottom-nav__link" href="#" id='link${i}'>${i}</a></li>`
+        if (i == idPagina){
+            const linkAtual = document.querySelector(`#link${i}`)
+            linkAtual.style = 'color: var(--red); text-decoration: underline'
+        }
+    }
+
+    if (rangeMax < Math.ceil(numTotal/4)){
+        navList.innerHTML += `<li class="bottom-nav__item"><a class="bottom-nav__link" href="#" id='+'>...</a></li>`
+    }
+
+    if (idPagina < Math.ceil(numTotal/4)){
+        navList.innerHTML += `<li class="bottom-nav__item"><a class="bottom-nav__link" href="#" id='f'>&gt;&gt;</a></li>`
+    }
+
+
+    const navItems = navList.children
+    console.log(navItems[0].firstChild)
+
+    navItems[0].firstChild.addEventListener('click', (e)=>{
+        e.preventDefault()
+        console.log('clicou')
+    })
+
+    for (let i = 0; i < navItems.length; i++) {
+        navItems[i].firstChild.addEventListener('click', (e)=>{
+            e.preventDefault()
+            
+            if (e.target.id != '+' && e.target.id != '-' && e.target.id != 'i' && e.target.id != 'f'){
+                paginaAtual = parseInt(e.target.text)
+            }
+
+            if(e.target.id == '+'){
+                paginaAtual = rangeMax + 1
+            }
+            if(e.target.id == '-'){
+                paginaAtual = rangeMin - 1
+            }
+            if(e.target.id == 'i'){
+                paginaAtual = 1
+            }
+            if(e.target.id == 'f'){
+                paginaAtual = Math.ceil(numTotal/4)
+            }
+
+            carregarDados().then((lobos) => {
+                construirPagina(paginaAtual, lobos)
+        
+            });
+        })
+        
+    }
+}
+
+        // let loboSelecionado = JSON.stringify(lobos[0])
+        // localStorage.setItem('loboSelecionado', loboSelecionado)
+        // console.log(localStorage.getItem('loboSelecionado'))
+        // loboRetornado = JSON.parse(localStorage.getItem('loboSelecionado'))
+        // console.log(loboRetornado)
+
+
+
+
+
+
