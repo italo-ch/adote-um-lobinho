5af2bc3dc9139665ff2792718ae0abe03e3b8eb8
Bruno Coitinho <bruno.pereira1988@gmail.com>
2025-02-11 14:08:57 -0300

    feat: filtragem por digitação de nome finalizada

--- scripts/search-filter.js
+++ scripts/search-filter.js
@@ -1,13 +1,11 @@
 const navList = document.querySelector('.bottom-nav__list')
+const searchBar = document.querySelector('#searchName')
 
 
 document.addEventListener('DOMContentLoaded', (e) =>{
-
-    
     
     
     let paginaAtual = 1;
-    let paginaAntesDoClick = paginaAtual
 
     
     
@@ -15,6 +13,18 @@ document.addEventListener('DOMContentLoaded', (e) =>{
     carregarDados().then((lobos) => {
         construirPagina(paginaAtual, lobos)
 
+    });
+
+    searchBar.addEventListener("keyup", (e) => {
+        carregarDados().then((lobos) => {
+            let filtrado = lobos.filter((elem) =>
+                elem.nome.toLowerCase().includes(searchBar.value.toLowerCase())
+            );
+    
+            paginaAtual = 1;
+            construirPagina(paginaAtual, filtrado);
+            // construirNavButtons(paginaAtual, filtrado);
+        });
     });
 
 })
@@ -32,6 +42,7 @@ async function carregarDados() {
 function construirPagina(idPagina, dados){
     const containerLobos = document.querySelector("#lobos-container");
     containerLobos.innerHTML = ''
+    let reverse = ''
     for (let i = idPagina * 4 - 4; i < 4 * idPagina; i++) {
 
         if ((i+1) % 2 == 0){
@@ -40,6 +51,7 @@ function construirPagina(idPagina, dados){
             reverse = ''
         }
 
+        if (i >= dados.length) break;
         containerLobos.innerHTML += `
         <div class="card-example ${reverse}" id='${dados[i].id}'>
                     <div class="image-container">
@@ -67,80 +79,104 @@ function construirPagina(idPagina, dados){
                 </div>`
     }
 
-    construirNavButtons(idPagina, dados.length)
+    construirNavButtons(idPagina, dados)
 }
 
-function construirNavButtons(idPagina, numTotal){
-    paginaAntesDoClick = idPagina
+function construirNavButtons(idPagina, dados){
     navList.innerHTML = ''
     let rangeMin = 1
 
-    if (idPagina != 1){
-        navList.innerHTML += `<li class="bottom-nav__item"><a class="bottom-nav__link" href="#" id='i'>&lt;&lt;</a></li>`
-    }
+    console.log(`dados filtrados: ${dados.length}`)
 
-
-    if (idPagina >3){
-        rangeMin = idPagina - 2
-        navList.innerHTML += `<li class="bottom-nav__item"><a class="bottom-nav__link" href="#" id='-'>...</a></li>`
-    }
-
-    let rangeMax = Math.min(rangeMin + 4,Math.ceil(numTotal/4))
-
-
-    
-    for (let i = rangeMin; i <= rangeMax; i++) {
+    if(dados.length <= 4){
+        let i = 1
         navList.innerHTML += `<li class="bottom-nav__item"><a class="bottom-nav__link" href="#" id='link${i}'>${i}</a></li>`
         if (i == idPagina){
             const linkAtual = document.querySelector(`#link${i}`)
             linkAtual.style = 'color: var(--red); text-decoration: underline'
         }
-    }
+    } else{
+        if (idPagina != 1){
+            navList.innerHTML += `<li class="bottom-nav__item"><a class="bottom-nav__link" href="#" id='i'>&lt;&lt;</a></li>`
+        }
+    
+    
+        if (idPagina >3){
+            rangeMin = idPagina - 2
+            navList.innerHTML += `<li class="bottom-nav__item"><a class="bottom-nav__link" href="#" id='-'>...</a></li>`
+        }
+    
+        let rangeMax = Math.min(rangeMin + 4,Math.ceil(dados.length/4))
+    
+    
+        
+        for (let i = rangeMin; i <= rangeMax; i++) {
+            navList.innerHTML += `<li class="bottom-nav__item"><a class="bottom-nav__link" href="#" id='link${i}'>${i}</a></li>`
+            if (i == idPagina){
+                const linkAtual = document.querySelector(`#link${i}`)
+                linkAtual.style = 'color: var(--red); text-decoration: underline'
+            }
+        }
+    
+        if (rangeMax < Math.ceil(dados.length/4)){
+            navList.innerHTML += `<li class="bottom-nav__item"><a class="bottom-nav__link" href="#" id='+'>...</a></li>`
+        }
+    
+        if (idPagina < Math.ceil(dados.length/4)){
+            navList.innerHTML += `<li class="bottom-nav__item"><a class="bottom-nav__link" href="#" id='f'>&gt;&gt;</a></li>`
+        }
+    
+    
+        const navItems = navList.children
+        
+        // for (let i = 0; i < navItems.length; i++) {
+        //     console.log(navItems[i].firstChild)
+        // }
 
-    if (rangeMax < Math.ceil(numTotal/4)){
-        navList.innerHTML += `<li class="bottom-nav__item"><a class="bottom-nav__link" href="#" id='+'>...</a></li>`
-    }
-
-    if (idPagina < Math.ceil(numTotal/4)){
-        navList.innerHTML += `<li class="bottom-nav__item"><a class="bottom-nav__link" href="#" id='f'>&gt;&gt;</a></li>`
-    }
+        const navLinks = document.querySelectorAll('.bottom-nav__link')
 
 
-    const navItems = navList.children
-    console.log(navItems[0].firstChild)
+        for (let i = 0; i < navItems.length; i++) {
+            navLinks[i].addEventListener('click', (e)=>{
+                e.preventDefault()
 
-    navItems[0].firstChild.addEventListener('click', (e)=>{
-        e.preventDefault()
-        console.log('clicou')
-    })
-
-    for (let i = 0; i < navItems.length; i++) {
-        navItems[i].firstChild.addEventListener('click', (e)=>{
-            e.preventDefault()
+                console.log(`clicou no ${e.target.text}`)
+                
+                if (e.target.id != '+' && e.target.id != '-' && e.target.id != 'i' && e.target.id != 'f'){
+                    paginaAtual = parseInt(e.target.text)
+                }
+    
+                if(e.target.id == '+'){
+                    paginaAtual = rangeMax + 1
+                }
+                if(e.target.id == '-'){
+                    paginaAtual = rangeMin - 1
+                }
+                if(e.target.id == 'i'){
+                    paginaAtual = 1
+                }
+                if(e.target.id == 'f'){
+                    paginaAtual = Math.ceil(dados.length/4)
+                }
+    
+                carregarDados().then((lobos) => {
+                    if (dados.length == lobos.length){
+                        construirPagina(paginaAtual, lobos)
+                    }else{
+                        console.log(dados.length)
+                        construirPagina(paginaAtual, dados)
+                    }
             
-            if (e.target.id != '+' && e.target.id != '-' && e.target.id != 'i' && e.target.id != 'f'){
-                paginaAtual = parseInt(e.target.text)
-            }
+                });
+            })
+            
+        }
+    
+    
 
-            if(e.target.id == '+'){
-                paginaAtual = rangeMax + 1
-            }
-            if(e.target.id == '-'){
-                paginaAtual = rangeMin - 1
-            }
-            if(e.target.id == 'i'){
-                paginaAtual = 1
-            }
-            if(e.target.id == 'f'){
-                paginaAtual = Math.ceil(numTotal/4)
-            }
 
-            carregarDados().then((lobos) => {
-                construirPagina(paginaAtual, lobos)
-        
-            });
-        })
-        
+
+    
     }
 }
 
@@ -149,9 +185,3 @@ function construirNavButtons(idPagina, numTotal){
         // console.log(localStorage.getItem('loboSelecionado'))
         // loboRetornado = JSON.parse(localStorage.getItem('loboSelecionado'))
         // console.log(loboRetornado)
-
-
-
-
-
-
